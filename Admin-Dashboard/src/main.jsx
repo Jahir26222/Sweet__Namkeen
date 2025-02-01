ed96e6e760f5ebd47ad6e2b6ff3db73192527eb6
Jahir Hussain <143325479+Jahir26222@users.noreply.github.com>
2025-02-01 22:45:39 +0530

    Update main.jsx

--- Admin-Dashboard/src/main.jsx
+++ Admin-Dashboard/src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter
+    basename = {process.env.PUBLIC_URL}
+    >
     <App />
   </BrowserRouter>
-)
+  document.getElementById('root')
+);
